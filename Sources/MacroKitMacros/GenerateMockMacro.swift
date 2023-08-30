766e192f14c2f73614f46a4ef841d11f3304613c
Brandon Williams <mbrandonw@hey.com>
2023-08-30 12:38:20 -0400

    wip

--- Sources/MacroKitMacros/GenerateMockMacro.swift
+++ Sources/MacroKitMacros/GenerateMockMacro.swift
@@ -1,109 +1,119 @@
 public struct GenerateMockMacro: PeerMacro {
-    enum Error: String, Swift.Error, DiagnosticMessage {
-        var diagnosticID: MessageID { .init(domain: "GenerateMockMacro", id: rawValue) }
-        var severity: DiagnosticSeverity { .error }
-        var message: String {
-            switch self {
-            case .notAProtocol: return "@GenerateMock can only be applied to protocols"
-            }
-        }
-
-        case notAProtocol
+  enum Error: String, Swift.Error, DiagnosticMessage {
+    var diagnosticID: MessageID { .init(domain: "GenerateMockMacro", id: rawValue) }
+    var severity: DiagnosticSeverity { .error }
+    var message: String {
+      switch self {
+      case .notAProtocol: return "@GenerateMock can only be applied to protocols"
+      }
     }
 
-    public static func expansion<Context: MacroExpansionContext, Declaration: DeclSyntaxProtocol>(
-        of node: AttributeSyntax,
-        providingPeersOf declaration: Declaration,
-        in context: Context
-    ) throws -> [DeclSyntax] {
-        guard let protoDecl = declaration.as(ProtocolDeclSyntax.self) else { throw Error.notAProtocol }
+    case notAProtocol
+  }
 
+  public static func expansion<Context: MacroExpansionContext, Declaration: DeclSyntaxProtocol>(
+    of node: AttributeSyntax,
+    providingPeersOf declaration: Declaration,
+    in context: Context
+  ) throws -> [DeclSyntax] {
+    guard let protoDecl = declaration.as(ProtocolDeclSyntax.self) else { throw Error.notAProtocol }
 
-        // Instance properties
-        let mockMemberProperties = protoDecl.properties
-            .map { DeclSyntax("public var \(raw: $0.identifier.text): MockMember<\(raw: $0.type!.type.trimmed), \(raw: $0.returnType)> = .init()") }
-            .compactMap { MemberDeclListItemSyntax(decl: $0) }
-
-        let properties = protoDecl.properties
-            .map(\.mockProperty)
-            .compactMap { MemberDeclListItemSyntax(decl: $0) }
-
-        // Instance functions
-        let mockMemberFunctions = protoDecl.functions
-            .map { DeclSyntax("public var \(raw: $0.identifier.text): MockMember<(\(raw: $0.parameters.typesWithoutAttribues.map(\.description).joined(separator: ", "))), \(raw: $0.returnTypeOrVoid)> = .init()") }
-            .compactMap { MemberDeclListItemSyntax(decl: $0) }
-
-        let functions = protoDecl.functions
-            .map(\.mockFunction)
-            .compactMap { MemberDeclListItemSyntax(decl: $0) }
-
-
-        // Consolidation
-        let mockMemberMembers: MemberDeclListSyntax = .init(mockMemberProperties + mockMemberFunctions)
-
-        let mockMembers = ClassDeclSyntax(
-            modifiers: ModifierListSyntax {
-                DeclModifierSyntax(name: "public")
-            },
-            identifier: "Members",
-            memberBlock: MemberDeclBlockSyntax(members: mockMemberMembers)
+    // Instance properties
+    let mockMemberProperties = protoDecl.properties
+      .map {
+        DeclSyntax(
+          "public var \(raw: $0.identifier.text): MockMember<\(raw: $0.type!.type.trimmed), \(raw: $0.returnType)> = .init()"
         )
+      }
+      .compactMap { MemberBlockItemSyntax(decl: $0) }
 
-        // Associatedtypes
-        var genericParams: GenericParameterClauseSyntax?
-        let associatedTypes = protoDecl.associatedTypes
-        if !associatedTypes.isEmpty {
-            let params = protoDecl.associatedTypes.enumerated().map { x, type in
-                return type.genericParameter.with(\.trailingComma, x == associatedTypes.count - 1 ? nil : .commaToken())
-            }
-            genericParams = .init(genericParameterList: .init(params))
+    let properties = protoDecl.properties
+      .map(\.mockProperty)
+      .compactMap { MemberBlockItemSyntax(decl: $0) }
+
+    // Instance functions
+    let mockMemberFunctions = protoDecl.functions
+      .map {
+        DeclSyntax(
+          "public var \(raw: $0.name.text): MockMember<(\(raw: $0.parameters.typesWithoutAttribues.map(\.description).joined(separator: ", "))), \(raw: $0.returnTypeOrVoid)> = .init()"
+        )
+      }
+      .compactMap { MemberBlockItemSyntax(decl: $0) }
+
+    let functions = protoDecl.functions
+      .map(\.mockFunction)
+      .compactMap { MemberBlockItemSyntax(decl: $0) }
+
+    // Consolidation
+    let mockMemberMembers: MemberBlockItemListSyntax = .init(mockMemberProperties + mockMemberFunctions)
+
+    let mockMembers = ClassDeclSyntax(
+      modifiers: DeclModifierListSyntax {
+        DeclModifierSyntax(name: "public")
+      },
+      name: "Members",
+      memberBlock: MemberBlockSyntax(members: mockMemberMembers)
+    )
+
+    // Associatedtypes
+    var genericParams: GenericParameterClauseSyntax?
+    let associatedTypes = protoDecl.associatedTypes
+    if !associatedTypes.isEmpty {
+      let params = protoDecl.associatedTypes.enumerated().map { x, type in
+        return type.genericParameter.with(
+          \.trailingComma, x == associatedTypes.count - 1 ? nil : .commaToken())
+      }
+      genericParams = GenericParameterClauseSyntax(parameters: .init(params))
+    }
+
+
+    let cls = try ClassDeclSyntax(
+      modifiers: DeclModifierListSyntax {
+        DeclModifierSyntax(name: "open")
+      },
+      name: "\(raw: protoDecl.name.text)Mock",
+      genericParameterClause: genericParams,
+      inheritanceClause: InheritanceClauseSyntax {
+        InheritedTypeSyntax(type: TypeSyntax("\(raw: protoDecl.name.text)"))
+        if let inheritance = protoDecl.inheritanceClause?.inheritedTypes {
+          inheritance
+        }
+      },
+      genericWhereClause: nil,
+      memberBlockBuilder: {
+        DeclSyntax("public let mocks = Members()")
+
+        mockMembers
+
+        let initializers = protoDecl.initializers
+        if initializers.isEmpty {
+          DeclSyntax("public init() {}")
+        }
+        for initializer in initializers {
+          try InitializerDeclSyntax(validating: initializer)
+            .with(
+              \.body,
+               CodeBlockSyntax {
+                 DeclSyntax("// ")
+               })
         }
 
-        let cls = try ClassDeclSyntax(
-            modifiers: ModifierListSyntax {
-                DeclModifierSyntax(name: "open")
-            },
-            identifier: "\(raw: protoDecl.identifier.text)Mock",
-            genericParameterClause: genericParams,
-            inheritanceClause: TypeInheritanceClauseSyntax {
-                InheritedTypeSyntax(typeName: TypeSyntax("\(raw: protoDecl.identifier.text)"))
-                if let inheritance = protoDecl.inheritanceClause?.inheritedTypeCollection {
-                    inheritance
-                }
-            },
-            genericWhereClause: nil,
-            memberBlockBuilder: {
-                DeclSyntax("public let mocks = Members()")
+        MemberBlockItemListSyntax(properties)
+        MemberBlockItemListSyntax(functions)
+      }
+    )
 
-                mockMembers
-
-                let initializers = protoDecl.initializers
-                if initializers.isEmpty {
-                    DeclSyntax("public init() {}")
-                }
-                for initializer in initializers {
-                    try InitializerDeclSyntax(validating: initializer)
-                        .with(\.body, CodeBlockSyntax {
-                            DeclSyntax("// ")
-                        })
-                }
-
-              MemberBlockItemListSyntax(properties)
-              MemberBlockItemListSyntax(functions)
-            }
-        )
-
-        return [
-            "#if DEBUG",
-            DeclSyntax(cls),
-            "#endif"
-        ]
-    }
+    return [
+      "#if DEBUG",
+      DeclSyntax(cls),
+      "#endif",
+    ]
+  }
 }
 
-private extension VariableDeclSyntax {
-    /// Take a `VariableDeclSyntax` from the source protocol and add `AccessorDeclSyntax`s for the getter and, if needed, setter
-  var mockProperty: VariableDeclSyntax {
+extension VariableDeclSyntax {
+  /// Take a `VariableDeclSyntax` from the source protocol and add `AccessorDeclSyntax`s for the getter and, if needed, setter
+  fileprivate var mockProperty: VariableDeclSyntax {
     var newProperty = trimmed
     var binding = newProperty.bindings.first!
     let accessor = binding.accessorBlock!.as(AccessorBlockSyntax.self)!
@@ -114,7 +124,9 @@ private extension VariableDeclSyntax {
     else { fatalError() }
 
     getter.body = CodeBlockSyntax {
-      DeclSyntax("\(raw: getter.effectSpecifiers?.throwsSpecifier != nil ? "try " : "")mocks.\(raw: newProperty.identifier.text).getter()")
+      DeclSyntax(
+        "\(raw: getter.effectSpecifiers?.throwsSpecifier != nil ? "try " : "")mocks.\(raw: newProperty.identifier.text).getter()"
+      )
     }
 
     accessors = [getter]
@@ -127,80 +139,60 @@ private extension VariableDeclSyntax {
     newProperty.bindings[newProperty.bindings.startIndex] = binding
     return newProperty.trimmed
   }
-
-//  {
-//        var newProperty = trimmed
-//        var binding = newProperty.bindings.first!
-//      let accessor = binding.accessorBlock!.as(AccessorBlockSyntax.self)!
-//      var getter = accessor.accessors.trimmed
-//
-////      getter = .accessors(
-////        AccessorDeclListSyntax {
-////          AccessorDeclListSyntax(
-////            CodeBlockSyntax {
-////              DeclSyntax("\(raw: self.effectSpecifiers?.throwsSpecifier != nil ? "try " : "")mocks.\(raw: newProperty.identifier.text).getter()")
-////            }
-////          )!
-////        }
-////      )
-////
-////
-////        var accessors: [AccessorDeclSyntax] = [getter]
-////        if getter.effectSpecifiers == nil {
-////            accessors.append("set { mocks.\(raw: identifier.text).setter(newValue) }")
-////        }
-////
-////        binding.accessor = .accessors(.init(accessors: .init(accessors)))
-////        newProperty.accessLevel = .open
-////        newProperty.bindings = newProperty.bindings.replacing(childAt: 0, with: binding)
-//        return newProperty.trimmed
-//    }
 }
 
-private extension FunctionDeclSyntax {
-    var mockFunction: FunctionDeclSyntax {
-        var newFunction = trimmed
+extension FunctionDeclSyntax {
+  fileprivate var mockFunction: FunctionDeclSyntax {
+    var newFunction = trimmed
 
-        var newSignature = signature
-        var params: [String] = []
-      for (x, param) in signature.parameterClause.parameters.enumerated() {
-            var newParam = param
-            newParam.secondName = "arg\(raw: x)"
-        newSignature.parameterClause.parameters = newSignature.parameterClause.parameters.replacing(childAt: x, with: newParam)
+    var newSignature = signature
+    var params: [String] = []
+    for (x, param) in signature.parameterClause.parameters.enumerated() {
+      var newParam = param
+      newParam.secondName = "arg\(raw: x)"
+      newSignature.parameterClause.parameters[newSignature.parameterClause.parameters.index(newSignature.parameterClause.parameters.startIndex, offsetBy: x)] = newParam
 
-            params.append("arg\(x)")
-        }
-
-        newFunction.signature = newSignature
-        newFunction.accessLevel = .open
-        newFunction.body = CodeBlockSyntax {
-          DeclSyntax("return \(raw: isThrowing ? "try " : "")mocks.\(raw: name.text).execute((\(raw: params.joined(separator: ", "))))")
-        }
-        return newFunction
+      params.append("arg\(x)")
     }
+
+    newFunction.signature = newSignature
+    newFunction.accessLevel = .open
+    newFunction.body = CodeBlockSyntax {
+      DeclSyntax(
+        "return \(raw: isThrowing ? "try " : "")mocks.\(raw: name.text).execute((\(raw: params.joined(separator: ", "))))"
+      )
+    }
+    return newFunction
+  }
 }
 
-private extension VariableDeclSyntax {
-    var returnType: DeclSyntax {
-        if isThrowing { return "\(raw: "Result<\(type!.type.trimmed), Error>")" }
-        else { return "\(raw: type!.type.trimmed)" }
+extension VariableDeclSyntax {
+  fileprivate var returnType: DeclSyntax {
+    if isThrowing {
+      return "\(raw: "Result<\(type!.type.trimmed), Error>")"
+    } else {
+      return "\(raw: type!.type.trimmed)"
     }
+  }
 }
-private extension FunctionDeclSyntax {
-    var returnTypeOrVoid: DeclSyntax {
-      if isThrowing { return "Result<\(raw: returnOrVoid.type), Error>" }
-      else { return "\(raw: returnOrVoid.type)" }
+extension FunctionDeclSyntax {
+  fileprivate var returnTypeOrVoid: DeclSyntax {
+    if isThrowing {
+      return "Result<\(raw: returnOrVoid.type), Error>"
+    } else {
+      return "\(raw: returnOrVoid.type)"
     }
+  }
 }
-private extension AssociatedTypeDeclSyntax {
-    var genericParameter: GenericParameterSyntax {
-      let type = self.inheritanceClause?.inheritedTypes.first
-        
-        return GenericParameterSyntax(
-            attributes: attributes,
-            name: name,
-            colon: type.map { _ in .colonToken() },
-            inheritedType: type.map { TypeSyntax("\(raw: $0)") }
-        )
-    }
+extension AssociatedTypeDeclSyntax {
+  fileprivate var genericParameter: GenericParameterSyntax {
+    let type = self.inheritanceClause?.inheritedTypes.first
+
+    return GenericParameterSyntax(
+      attributes: attributes,
+      name: name,
+      colon: type.map { _ in .colonToken() },
+      inheritedType: type.map { TypeSyntax("\(raw: $0)") }
+    )
+  }
 }
--- Sources/MacroKitMacros/Support/VariableDeclSyntax+Extensions.swift
+++ Sources/MacroKitMacros/Support/VariableDeclSyntax+Extensions.swift
@@ -1,178 +1,186 @@
 extension VariableDeclSyntax {
-    public var isComputed: Bool {
-      return bindings.contains(where: { $0.accessorBlock?.is(CodeBlockSyntax.self) == true })
-    }
-    public var isStored: Bool {
-        return !isComputed
-    }
-    public var isStatic: Bool {
-      return modifiers.lazy.contains(where: { $0.name.tokenKind == .keyword(.static) }) == true
-    }
-    public var identifier: TokenSyntax {
-        return bindings.lazy.compactMap({ $0.pattern.as(IdentifierPatternSyntax.self) }).first!.identifier
-    }
+  public var isComputed: Bool {
+    return bindings.contains(where: { $0.accessorBlock?.is(CodeBlockSyntax.self) == true })
+  }
+  public var isStored: Bool {
+    return !isComputed
+  }
+  public var isStatic: Bool {
+    return modifiers.lazy.contains(where: { $0.name.tokenKind == .keyword(.static) }) == true
+  }
+  public var identifier: TokenSyntax {
+    return bindings.lazy.compactMap({ $0.pattern.as(IdentifierPatternSyntax.self) }).first!
+      .identifier
+  }
 
-    public var type: TypeAnnotationSyntax? {
-        return bindings.lazy.compactMap(\.typeAnnotation).first
-    }
+  public var type: TypeAnnotationSyntax? {
+    return bindings.lazy.compactMap(\.typeAnnotation).first
+  }
 
-    public var initializerValue: ExprSyntax? {
-        return bindings.lazy.compactMap(\.initializer).first?.value
-    }
+  public var initializerValue: ExprSyntax? {
+    return bindings.lazy.compactMap(\.initializer).first?.value
+  }
 
-    public var effectSpecifiers: AccessorEffectSpecifiersSyntax? {
-        return bindings
-            .lazy
-            .compactMap(\.accessorBlock?.accessors)
-            .compactMap({ accessor in
-                switch accessor {
-                case .accessors(let syntax):
-                  return syntax.lazy.compactMap(\.effectSpecifiers).first
-                case .getter:
-                    return nil
-                }
-            })
-            .first
-    }
-    public var isThrowing: Bool {
-        return bindings
+  public var effectSpecifiers: AccessorEffectSpecifiersSyntax? {
+    return bindings
+      .lazy
+      .compactMap(\.accessorBlock?.accessors)
+      .compactMap({ accessor in
+        switch accessor {
+        case .accessors(let syntax):
+          return syntax.lazy.compactMap(\.effectSpecifiers).first
+        case .getter:
+          return nil
+        }
+      })
+      .first
+  }
+  public var isThrowing: Bool {
+    return
+      bindings
+      .compactMap(\.accessorBlock?.accessors)
+      .contains(where: { accessor in
+        switch accessor {
+        case .accessors(let syntax):
+          return syntax.contains(where: { $0.effectSpecifiers?.throwsSpecifier != nil })
+        case .getter:
+          return false
+        }
+      })
+  }
+  public var isAsync: Bool {
+    return
+      bindings
+      .compactMap(\.accessorBlock?.accessors)
+      .contains(where: { accessor in
+        switch accessor {
+        case .accessors(let syntax):
+          return syntax.lazy.contains(where: { $0.effectSpecifiers?.asyncSpecifier != nil })
+        case .getter:
+          return false
+        }
+      })
+  }
+
+  public var getter: AccessorDeclSyntax? {
+    get {
+      return bindings
+        .lazy
         .compactMap(\.accessorBlock?.accessors)
-            .contains(where: { accessor in
-                switch accessor {
-                case .accessors(let syntax):
-                    return syntax.contains(where: { $0.effectSpecifiers?.throwsSpecifier != nil })
-                case .getter:
-                    return false
-                }
-            })
-    }
-    public var isAsync: Bool {
-        return bindings
-            .compactMap(\.accessorBlock?.accessors)
-            .contains(where: { accessor in
-                switch accessor {
-                case .accessors(let syntax):
-                    return syntax.lazy.contains(where: { $0.effectSpecifiers?.asyncSpecifier != nil })
-                case .getter:
-                    return false
-                }
-            })
-    }
+        .compactMap { accessor in
+          switch accessor {
+          case .getter(let body):
+            var getter = AccessorDeclSyntax(
+              accessorSpecifier: .keyword(.get), body: CodeBlockSyntax(body))
+            getter.modifier = DeclModifierSyntax(
+              name: TokenSyntax(stringLiteral: accessLevel.rawValue))
+            return getter
 
-    public var getter: AccessorDeclSyntax? {
-        get {
-            return bindings
-                .lazy
-                .compactMap(\.accessorBlock?.accessors)
-                .compactMap { accessor in
-                    switch accessor {
-                    case .getter(let body):
-                      var getter = AccessorDeclSyntax(accessorSpecifier: .keyword(.get), body: CodeBlockSyntax(body))
-                        getter.modifier = DeclModifierSyntax(name: TokenSyntax(stringLiteral: accessLevel.rawValue))
-                        return getter
-
-                    case .accessors(let block):
-                      return block.lazy.first(where: { $0.accessorSpecifier.tokenKind == .keyword(.get) })?.trimmed
-                    }
-                }
-                .first
+          case .accessors(let block):
+            return block.lazy.first(where: { $0.accessorSpecifier.tokenKind == .keyword(.get) })?
+              .trimmed
+          }
         }
-        set {
-            guard let newValue else { fatalError("Removing getters is not supported") }
+        .first
+    }
+    set {
+      guard let newValue else { fatalError("Removing getters is not supported") }
 
-            for (x, var binding) in bindings.enumerated() {
-              guard var accessor = binding.accessorBlock?.accessors else { continue }
+      for (x, var binding) in bindings.enumerated() {
+        guard var accessor = binding.accessorBlock?.accessors else { continue }
 
-                switch accessor {
-                case .getter:
-                  accessor = .accessors(.init([newValue]))
-                  binding.accessorBlock?.accessors = accessor
-                    bindings = bindings.replacing(childAt: x, with: binding)
-                    return
+        switch accessor {
+        case .getter:
+          accessor = .accessors(.init([newValue]))
+          binding.accessorBlock?.accessors = accessor
+          bindings[bindings.index(bindings.startIndex, offsetBy: x)] = binding
+          return
 
-                case .accessors(var block):
-                    var update = Array(block.lazy)
-                    for (index, accessor) in block.lazy.enumerated() {
-                      if accessor.accessorSpecifier.tokenKind == .keyword(.get) {
-                        update[index] = newValue
-//                            update = update.replacing(childAt: index, with: newValue)
-                        } else {
-                          update[index] = accessor.trimmed
-//                            update = update.replacing(childAt: index, with: accessor.trimmed)
-                        }
-                    }
-
-                  block = .init(update)
-                    accessor = .accessors(block)
-                  binding.accessorBlock?.accessors = accessor
-                    bindings = bindings.replacing(childAt: x, with: binding)
-                    return
-                }
+        case .accessors(var block):
+          var update = Array(block.lazy)
+          for (index, accessor) in block.lazy.enumerated() {
+            if accessor.accessorSpecifier.tokenKind == .keyword(.get) {
+              update[index] = newValue
+              //                            update = update.replacing(childAt: index, with: newValue)
+            } else {
+              update[index] = accessor.trimmed
+              //                            update = update.replacing(childAt: index, with: accessor.trimmed)
             }
+          }
 
-          bindings[bindings.startIndex].accessorBlock?.accessors = .accessors([newValue])
+          block = .init(update)
+          accessor = .accessors(block)
+          binding.accessorBlock?.accessors = accessor
+          bindings[bindings.index(bindings.startIndex, offsetBy: x)] = binding
+          return
         }
+      }
+
+      bindings[bindings.startIndex].accessorBlock?.accessors = .accessors([newValue])
     }
-    public var setter: AccessorDeclSyntax? {
-        get {
-            return bindings
-                .lazy
-                .compactMap(\.accessorBlock?.accessors)
-                .compactMap { accessor in
-                    switch accessor {
-                    case .getter:
-                        return nil
+  }
+  public var setter: AccessorDeclSyntax? {
+    get {
+      return bindings
+        .lazy
+        .compactMap(\.accessorBlock?.accessors)
+        .compactMap { accessor in
+          switch accessor {
+          case .getter:
+            return nil
 
-                    case .accessors(let block):
-                      return block.lazy.first(where: { $0.accessorSpecifier.tokenKind == .keyword(.set) })?.trimmed
-                    }
-                }
-                .first
+          case .accessors(let block):
+            return block.lazy.first(where: { $0.accessorSpecifier.tokenKind == .keyword(.set) })?
+              .trimmed
+          }
         }
-        set {
-            for (x, var binding) in bindings.enumerated() {
-              guard var accessor = binding.accessorBlock?.accessors else { continue }
+        .first
+    }
+    set {
+      for (x, var binding) in bindings.enumerated() {
+        guard var accessor = binding.accessorBlock?.accessors else { continue }
 
-                switch accessor {
-                case .getter(let body):
-                    guard let newValue else { return }
+        switch accessor {
+        case .getter(let body):
+          guard let newValue else { return }
 
-                  accessor = .accessors(.init([
-                    AccessorDeclSyntax(accessorSpecifier: .keyword(.get), body: .init(statements: body)),
-                        newValue
-                    ]))
-                  binding.accessorBlock?.accessors = accessor
-                    bindings = bindings.replacing(childAt: x, with: binding)
-                    return
+          accessor = .accessors(
+            .init([
+              AccessorDeclSyntax(accessorSpecifier: .keyword(.get), body: .init(statements: body)),
+              newValue,
+            ]))
+          binding.accessorBlock?.accessors = accessor
+          bindings[bindings.index(bindings.startIndex, offsetBy: x)] = binding
+          return
 
-                case .accessors(var block):
-                    var update = Array(block.lazy)
-                    for (index, accessor) in block.lazy.enumerated() {
-                        if accessor.accessorKind.tokenKind == .keyword(.set) {
-                            if let newValue {
-                              update[index] = newValue
-//                                update = update.replacing(childAt: index, with: newValue)
-                            } else {
-                              update.remove(at: index)
-//                                update = update.removing(childAt: index)
-                            }
-                        } else {
-                          update[index] = accessor.trimmed
-//                            update = update.replacing(childAt: index, with: accessor.trimmed)
-                        }
-                    }
-
-                    if update.count == 1, let newValue {
-                        update = update + [newValue]
-                    }
-
-                  block = .init(update)
-                    accessor = .accessors(block)
-                  binding.accessorBlock?.accessors = accessor
-                    bindings = bindings.replacing(childAt: x, with: binding)
-                    return
-                }
+        case .accessors(var block):
+          var update = Array(block.lazy)
+          for (index, accessor) in block.lazy.enumerated() {
+            if accessor.accessorSpecifier.tokenKind == .keyword(.set) {
+              if let newValue {
+                update[index] = newValue
+                //                                update = update.replacing(childAt: index, with: newValue)
+              } else {
+                update.remove(at: index)
+                //                                update = update.removing(childAt: index)
+              }
+            } else {
+              update[index] = accessor.trimmed
+              //                            update = update.replacing(childAt: index, with: accessor.trimmed)
             }
+          }
+
+          if update.count == 1, let newValue {
+            update = update + [newValue]
+          }
+
+          block = .init(update)
+          accessor = .accessors(block)
+          binding.accessorBlock?.accessors = accessor
+          bindings[bindings.index(bindings.startIndex, offsetBy: x)] = binding
+          return
         }
+      }
     }
+  }
 }
